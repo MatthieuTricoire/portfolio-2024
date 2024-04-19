6398fb9c4bc39c794185aedc00d4d9c773dc85d8
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-04-19 10:53:29 +0100

    update blob opacity

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body
         className={`${inter.className}  bg-slate-50 text-gray-900 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[10rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[80rem] dark:bg-[#946263]" />
-        <div className="bg-[#dbd7fb] absolute top-[-2rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
+        <div className="bg-[#fbe2e3] absolute top-[10rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[50rem] sm:w-[80rem] dark:bg-[#946263] dark:bg-opacity-50" />
+        <div className="bg-[#dbd7fb] absolute top-[-2rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[50rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394] dark:bg-opacity-50" />
         <ActiveSectionContextProvider>
           <ThemeContextProvider>
             <Header />
